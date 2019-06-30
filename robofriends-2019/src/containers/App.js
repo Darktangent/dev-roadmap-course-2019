5f8ef92d2fa170d6fdeda2310f4fdb32efdad72f
Ron Ganguly <c1ph3r9@gmail.com>
2019-06-30 16:41:36 -0500

    finished robo friends projects and build code

--- robofriends-2019/src/containers/App.js
+++ robofriends-2019/src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import CardList from "./CardList"
-import {robots} from "./robots"
-import SearchBox from "./SearchBox"
+import CardList from "../components/CardList"
+// import {robots} from "./robots"
+import SearchBox from "../components/SearchBox"
+import Scroll from "../components/Scroll"
 export default class App extends React.Component {
 	// static propTypes = {
 	// 	name: React.PropTypes.string,
@@ -10,10 +11,18 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state={
-			robots:robots,
+			robots:[],
 			searchField:""
 		}
 		this.onSearchChange=this.onSearchChange.bind(this)
+	}
+	componentDidMount(){
+		fetch(`https://jsonplaceholder.typicode.com/users`).then(response=>{
+			return response.json()
+		}).then(users=>{
+			this.setState({robots:users})
+		})
+		
 	}
 	onSearchChange(e){
 		this.setState({searchField:e.target.value})
@@ -25,12 +34,17 @@ export default class App extends React.Component {
 		const filteredRobots=this.state.robots.filter(robot=>{
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
 		})
+		if(this.state.robots.length===0){
+			return <h1>Loading</h1>
+		}else{
 		return (
 			<div className="tc">
-				<h1>Robofriends</h1>
+				<h1 className="heading-main">Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
 				<CardList robots={filteredRobots} />
+				</Scroll>
 			</div>
-		);
+		)}
 	}
 }
